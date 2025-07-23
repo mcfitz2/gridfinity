f480232c03693bccc9b4e9d0563ea19cd44f3f67
Micah Fitzgerald <mcfitz2@gmail.com>
2025-07-23 15:23:58 -0500

    skip entrypoint

--- build.sh
+++ build.sh
@@ -9,6 +9,10 @@ mkdir -p "$OUT_DIR"
 
 for scad_file in "$BIN_DIR"/*.scad; do
     base_name=$(basename "$scad_file" .scad)
+    # Skip entrypoint.scad
+    if [[ "$base_name" == "entrypoint" ]]; then
+        continue
+    fi
     stl_file="$OUT_DIR/$base_name.stl"
     echo "Building $scad_file -> $stl_file"
     openscad -o "$stl_file" "$scad_file"
